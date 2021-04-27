cb642397c1bb4dd9929edb7efc753e8054d325ef
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2021-04-27 11:49:46 +0700

    🚧  work in progress

--- src/main/java/com/example/simplespringboot/api/TestApi.java
+++ src/main/java/com/example/simplespringboot/api/TestApi.java
@@ -1,12 +1,29 @@
 package com.example.simplespringboot.api;
 
+import com.example.simplespringboot.business.TestBusiness;
 import com.example.simplespringboot.model.MRegisterRequest;
 import com.example.simplespringboot.model.TestResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/test")
 public class TestApi {
+
+    // method 1 Field Injection
+//    @Autowired
+//    private TestBusiness business;
+    // method 2 contructer Injection alt+enter
+    private  final TestBusiness business;
+
+    public TestApi(TestBusiness business) {
+        this.business = business;
+    }
+
+
     @GetMapping
     public TestResponse test(){
         TestResponse response = new TestResponse();
@@ -26,8 +43,14 @@ public class TestApi {
 
     @PostMapping
     @RequestMapping("/register")
-    public String register(@RequestBody MRegisterRequest request){
+    public ResponseEntity<String> register(@RequestBody MRegisterRequest request){
+        String res = null;
+        try{
+            res = business.register(request);
+        } catch (IOException e){
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).build();
+        }
 
-        return "post" + request;
+         return ResponseEntity.ok(res);
     }
 }
--- src/main/java/com/example/simplespringboot/business/TestBusiness.java
+++ src/main/java/com/example/simplespringboot/business/TestBusiness.java
@@ -3,9 +3,18 @@ package com.example.simplespringboot.business;
 import com.example.simplespringboot.model.MRegisterRequest;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
 public class TestBusiness {
-    public  String register(MRegisterRequest request){
-        return "";
+    public  String register(MRegisterRequest request) throws IOException {
+        if(request == null){
+            throw  new IOException("null.request");
+        }
+
+        if(request.getEmail()==null){
+            throw  new IOException("null.email");
+        }
+        return request.getName() ;
     }
 }
