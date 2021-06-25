3bba969245d62590c208a239dc1a9645441e847b
NantipatSoften <keymasterviriya1150@gmail.com>
2021-06-25 13:23:53 +0700

    will write test

--- frontend/src/app/services/user.service.spec.ts
+++ frontend/src/app/services/user.service.spec.ts
@@ -1,16 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
-
+  let email: string = 'Test@gmail.com';
+  let passward: string = 'Armlovemint1150';
+  let name: string = 'TestMan';
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(UserService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('register should return  email and name', () => {
+    service.register(email, passward, name).subscribe((response) => {
+      expect(response.email).toBe(email);
+      expect(response.name).toBe(name);
+    });
+  });
+
+  it('login should return token length 223 ', () => {
+    service.login(email, passward).subscribe((response) => {
+      expect(response.token.length).toBe(223);
+    });
   });
 });
